2eed5750992f173b6916fe3dc352f371e1dcb3ea
Warren Wilbee (W^3) <wwilbee@hotmail.com>
2012-01-26 19:44:38 +0000

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 var http = require("http");
+
+http.createServer(function(request, response) {
+    console.log ("request received");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Cloud City\n");
+    response.end();
+    console.log ("request processed");
+}).listen(process.env.PORT);
+console.log ("server is running");
