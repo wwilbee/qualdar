866f1042be9a07aeffb32a3df44e6906fc7b8d4d
Warren Wilbee (W^3) <wwilbee@hotmail.com>
2012-02-01 07:06:27 +0000

    post1

--- requestHandler.js
+++ requestHandler.js
@@ -1,13 +1,39 @@
-function start () {
+var querystring = require("querystring");
+
+function start (response,postData) {
     console.log ("Request handler 'start' was called.");
+    var body = 
+        '<html>'+
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; '+
+        'charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>'+
+        '</body>'+
+        '</html>';
+        
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(body);
+        response.end();
 }
 
-function upload () {
+
+function upload (response,postData) {
     console.log ("Request handler 'upload' was called.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("You have been sent this data: " + querystring.parse(postData).text);
+    response.end();
 }
 
-function fail () {
+function fail (response,PostData) {
     console.log ("Request Failed!");
+    response.writeHead(404, {"Content-Type": "text/plain"});    
+    response.write("404 Not found");    
+    response.end();
 }
 
 exports.start = start;
